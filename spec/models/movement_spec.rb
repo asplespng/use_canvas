55558a67f9132191345e6bc623dbc4665a239780
Per Evensen <per.evensen@gmail.com>
2016-08-20 09:06:01 -0400

    added first test

--- spec/models/movement_spec.rb
+++ spec/models/movement_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Movement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "does not allow other kinds" do
+    expect(FactoryGirl.build(:movement,kind: Movement.kinds[:bodyweight])).to be_valid
+    expect(FactoryGirl.build(:movement, kind: Movement.kinds[:test])).not_to be_valid
+  end
 end
